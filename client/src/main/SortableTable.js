860dc9c1113160b10e1e8b5ae6f5f08190a54fef
Luigi Cardito <lcardito@gmail.com>
2017-04-03 20:13:46 +0100

    Table must be stackable

--- client/src/main/SortableTable.js
+++ client/src/main/SortableTable.js
@@ -52,7 +52,7 @@ class SortableTable extends React.Component {
                    padded
                    sortable
                    striped
-                   unstackable
+                   stackable
                    selectable={this.state.editable}>
                 <Table.Header>
                     <Table.Row>
